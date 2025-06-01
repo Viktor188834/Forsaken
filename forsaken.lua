948ac112c5dfef4ea90eccc0e1b0c4fc9ad9a74d
Viktor188834 <atabaevviktor671@gmail.com>
2025-06-01 14:19:39 +0300

    Create forsaken.lua

---
+++ forsaken.lua
@@ -0,0 +1,30 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("c00lgui V0.8", "RJTheme4")
+
+local Tab = Window:NewTab("teleports (only 1 charecter in one team)")
+
+local Section = Tab:NewSection("teleport like 007n7")
+
+Section:NewButton("teleport like 007n7 (only chance)", "teleport like 007n7", function()
+  local randomNumber = math.random(1,6)
+  print(randomNumber)
+  wait(6)
+  workspace.Players.Survivors["chance"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
+end)
+
+Section:NewButton("teleport like 007n7(only guest)", "teleport like 007n7", function()
+  local randomNumber = math.random(1,6)
+  print(randomNumber)
+  wait(6)
+  workspace.Players.Survivors["Guest1337"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
+end)
+
+Section:NewButton("teleport like 007n7(only 007n7)", "teleport like 007n7", function()
+  local randomNumber = math.random(1,6)
+  print(randomNumber)
+  wait(6)
+  workspace.Players.Survivors["007n7"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
+end)
+local c00lgui = Window:CreateTab("c00lgui", nil) -- Title, Image
+local c00lguiSection = c00lgui:CreateSection("c00lgui")
