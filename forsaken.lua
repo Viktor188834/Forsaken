7b81617e497c822dff883fec8f4472601e63cf51
Viktor188834 <atabaevviktor671@gmail.com>
2025-06-01 17:00:23 +0300

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("c00lgui V1.00", "RJTheme4")
+local Window = Library.CreateLib("c00lgui V1.12", "RJTheme4")
 
 local Tab = Window:NewTab("c00lgui")
 
@@ -8,7 +8,14 @@ local Section = Tab:NewSection("teleport like 007n7")
 
 game:GetService("UserInputService").JumpRequest:connect(onJumpRequest)
 
--- телепорт как 007н7а
+-- телепорт как 007н7
+
+Section:NewButton("shedletsky teleport like 007n7", "teleport like 007n7", function()
+  local randomNumber = math.random(1,6)
+  print(randomNumber)
+  wait(6)
+  workspace.Players.Survivors["shedletsky"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
+end)
 
 Section:NewButton("chance teleport like 007n7", "teleport like 007n7", function()
   local randomNumber = math.random(1,6)
@@ -30,6 +37,19 @@ Section:NewButton("007n7 teleport like 007n7", "teleport like 007n7", function()
   wait(6)
   workspace.Players.Survivors["007n7"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
 end)
+
+--esp
+
+
+
+
+
+
+
+
+
+
+
 
 --телепортироватся к предмету
 
@@ -62,4 +82,68 @@ end)
 Section:NewButton("item Epicsauce teleport to you(only 007n7)", "item Epicsauce teleport to you", function()
   wait(3)
   workspace.Players.Survivors["007n7"]["Left Arm"].CFrame = workspace.Map.Ingame.Epicsauce.CFrame
+end)
+
+--телепортация к игрокам за джон до
+
+local Section = Tab:NewSection("john doe hunt")
+
+Section:NewButton("John Doe teleport to Noob", "John Doe teleport to Noob", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["Noob"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Elliot", "John Doe teleport to Elliot", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["Elliot"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Shedletsky", "John Doe teleport to Shedletsky", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["Shedletsky"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to 007n7", "John Doe teleport to 007n7", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["007n7"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Builderman", "John Doe teleport to Builderman", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["Builderman"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Guest1337", "John Doe teleport to Guest1337", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["Guest1337"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to TwoTime", "John Doe teleport to TwoTime", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["TwoTime"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Dusekkar", "John Doe teleport to Dusekkar", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["Dusekkar"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Taph", "John Doe teleport to Taph", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["Taph"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Chance", "John Doe teleport to Chance", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors["Chance"].Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Ena", "John Doe teleport to Ena", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors.Ena.Head.CFrame
+end)
+
+Section:NewButton("John Doe teleport to Taph", "John Doe teleport to Taph", function()
+  wait(0.5)
+  workspace.Players.Killers.JohnDoe["Right Leg"].CFrame = workspace.Players.Survivors.JohnDoe.Head.CFrame
 end)
