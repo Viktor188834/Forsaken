becef246dedd8ef8e1867f476ac9e8cbc7bc78d4
Viktor188834 <atabaevviktor671@gmail.com>
2025-06-01 15:35:57 +0300

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -1,28 +1,65 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("c00lgui V0.91", "RJTheme4")
+local Window = Library.CreateLib("c00lgui V1.00", "RJTheme4")
 
-local Tab = Window:NewTab("tps(only 1 charecter in one team)")
+local Tab = Window:NewTab("c00lgui")
 
 local Section = Tab:NewSection("teleport like 007n7")
 
-Section:NewButton("teleport like 007n7 (only chance)", "teleport like 007n7", function()
+game:GetService("UserInputService").JumpRequest:connect(onJumpRequest)
+
+-- телепорт как 007н7
+
+Section:NewButton("chance teleport like 007n7", "teleport like 007n7", function()
   local randomNumber = math.random(1,6)
   print(randomNumber)
   wait(6)
-  workspace.Players.Survivors["Chance"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
+  workspace.Players.Survivors["chance"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
 end)
 
-Section:NewButton("teleport like 007n7(only guest)", "teleport like 007n7", function()
+Section:NewButton("guest teleport like 007n7", "teleport like 007n7", function()
   local randomNumber = math.random(1,6)
   print(randomNumber)
   wait(6)
   workspace.Players.Survivors["Guest1337"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
 end)
 
-Section:NewButton("teleport like 007n7(only 007n7)", "teleport like 007n7", function()
+Section:NewButton("007n7 teleport like 007n7", "teleport like 007n7", function()
   local randomNumber = math.random(1,6)
   print(randomNumber)
   wait(6)
   workspace.Players.Survivors["007n7"]["Left Arm"].CFrame = workspace.Map.Ingame.Map.SpawnPoints.Survivors:GetChildren()[randomNumber].CFrame
+end)
+
+--телепортироватся к предмету
+
+local Section = Tab:NewSection("(3s)items tp to you")
+Section:NewButton("item medkit teleport to you(only guest)", "item medkit teleport to you", function()
+  wait(3)
+  workspace.Players.Survivors["Guest1337"]["Left Arm"].CFrame = workspace.Map.Ingame.Medkit.CFrame
+end)
+
+Section:NewButton("item BloxyCola teleport to you(only guest)", "item BloxyCola teleport to you", function()
+  wait(3)
+  workspace.Players.Survivors["Guest1337"]["Left Arm"].CFrame = workspace.Map.Ingame.BloxyCola.CFrame
+end)
+
+Section:NewButton("item Epicsauce teleport to you(only guest)", "item Epicsauce teleport to you", function()
+  wait(3)
+  workspace.Players.Survivors["Guest1337"]["Left Arm"].CFrame = workspace.Map.Ingame.Epicsauce.CFrame
+end)
+
+Section:NewButton("item medkit teleport to you(only 007n7)", "item medkit teleport to you", function()
+  wait(3)
+  workspace.Players.Survivors["007n7"]["Left Arm"].CFrame = workspace.Map.Ingame.Medkit.CFrame
+end)
+
+Section:NewButton("item BloxyCola teleport to you(only 007n7)", "item BloxyCola teleport to you", function()
+  wait(3)
+  workspace.Players.Survivors["007n7"]["Left Arm"].CFrame = workspace.Map.Ingame.BloxyCola.CFrame
+end)
+
+Section:NewButton("item Epicsauce teleport to you(only 007n7)", "item Epicsauce teleport to you", function()
+  wait(3)
+  workspace.Players.Survivors["007n7"]["Left Arm"].CFrame = workspace.Map.Ingame.Epicsauce.CFrame
 end)
