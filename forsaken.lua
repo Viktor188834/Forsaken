00ef001ef81c98be68bc612b136bb6aa4c826f7d
Viktor188834 <atabaevviktor671@gmail.com>
2025-06-03 21:40:55 +0300

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("c00lgui V2.15 ", "RJTheme4")
+local Window = Library.CreateLib("c00lgui V2.2 ", "RJTheme4")
 
 local Tab = Window:NewTab("c00lgui")
 
